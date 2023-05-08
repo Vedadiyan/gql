62f9ab295cae3545b2fa1809ff6563e83fe6c3c5
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-05-08 11:29:17 +0800

    mongo function

--- pkg/functions/mongo/mongo.go
+++ pkg/functions/mongo/mongo.go
@@ -3,8 +3,10 @@ package mongo
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
-	"fmt"
+	"sync"
 	"text/template"
 
 	cmn "github.com/vedadiyan/gql/pkg/common"
@@ -13,33 +15,22 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+type MongoArgs struct {
+	Collection *mongo.Collection
+	Query      bson.A
+}
+
 var (
 	_conManager func(connKey string) (*mongo.Client, error)
+	_templates  sync.Map
 )
 
 func Mongo(jo *[]any, row any, args []any) any {
-	result, err := readArgsGeneric(args, row, jo)
+	mongoArgs, err := readArgsGeneric(args, row, jo)
 	if err != nil {
 		return err
 	}
-	mapper := result.(map[string]any)
-	var buff bytes.Buffer
-	err = mapper["query"].(*template.Template).Execute(&buff, mapper["params"])
-	if err != nil {
-		return err
-	}
-	client, err := _conManager(mapper["connection"].(string))
-	if err != nil {
-		return err
-	}
-	databse := client.Database(mapper["database"].(string))
-	collection := databse.Collection(mapper["collection"].(string))
-	var filter bson.A
-	err = json.Unmarshal(buff.Bytes(), &filter)
-	if err != nil {
-		return err
-	}
-	res, err := collection.Aggregate(context.TODO(), filter)
+	res, err := mongoArgs.Collection.Aggregate(context.TODO(), mongoArgs.Query)
 	if err != nil {
 		return err
 	}
@@ -49,40 +40,77 @@ func Mongo(jo *[]any, row any, args []any) any {
 		json.Unmarshal([]byte(res.Current.String()), &dataMap)
 		data = append(data, dataMap)
 	}
-	fmt.Println(len(data))
 	return data
 }
-func readArgsGeneric(args []any, row any, jo *[]any) (any, error) {
-	mapper := make(map[string]any)
+func readArgsGeneric(args []any, row any, jo *[]any) (*MongoArgs, error) {
+	mongoArgs := MongoArgs{}
+	var conn *mongo.Client
+	var dbname string
+	var param any
 	connection := func(args any) error {
-		mapper["connection"] = args.(string)
-		return nil
-	}
-	database := func(args any) error {
-		mapper["database"] = args.(string)
-		return nil
-	}
-	collection := func(args any) error {
-		mapper["collection"] = args.(string)
-		return nil
-	}
-	params := func(args any) error {
-		mapper["params"] = args
-		return nil
-	}
-	query := func(args any) error {
-		res, err := template.New("").Parse(args.(string))
+		_conn, err := _conManager(args.(string))
 		if err != nil {
 			return err
 		}
-		mapper["query"] = res
+		conn = _conn
+		return nil
+	}
+	database := func(args any) error {
+		dbname = args.(string)
+		return nil
+	}
+	collection := func(args any) error {
+		mongoArgs.Collection = conn.Database(dbname).Collection(args.(string))
+		return nil
+	}
+	params := func(args any) error {
+		param = args
+		return nil
+	}
+	query := func(args any) error {
+		text := args.(string)
+		hash, err := getHash([]byte(text))
+		if err != nil {
+			return nil
+		}
+		var query *template.Template
+		oldQuery, ok := _templates.Load(hash)
+		if !ok {
+			query = oldQuery.(*template.Template)
+		} else {
+			newQuery, err := template.New(hash).Parse(args.(string))
+			if err != nil {
+				return err
+			}
+			query = newQuery
+			_templates.Store(hash, query)
+		}
+		var buff bytes.Buffer
+		err = query.Execute(&buff, param)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(buff.Bytes(), &mongoArgs.Query)
+		if err != nil {
+			return err
+		}
 		return nil
 	}
 	err := functions.CheckSingnature(args, []functions.ArgTypes{functions.STRING, functions.STRING, functions.STRING, functions.ANY, functions.STRING}, []functions.Reader{connection, database, collection, params, query})
 	if err != nil {
 		return nil, err
 	}
-	return mapper, nil
+	return &mongoArgs, nil
+}
+
+func getHash(bytes []byte) (string, error) {
+	sha := sha256.New()
+	_, err := sha.Write(bytes)
+	if err != nil {
+		return "", err
+	}
+	hash := base64.StdEncoding.EncodeToString(sha.Sum(nil))
+	return hash, nil
 }
 
 func RegisterConManager(fn func(connKey string) (*mongo.Client, error)) {
