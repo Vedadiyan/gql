bea885f7570a43fd74e65ab02bc986ebebf9c85e
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-05-27 22:04:34 +0800

    order by fix

--- pkg/sql/orderby.go
+++ pkg/sql/orderby.go
@@ -8,87 +8,208 @@ import (
 	"github.com/vedadiyan/gql/pkg/sentinel"
 )
 
-func orderBy(order map[string]bool, list []any) (err error) {
-	if len(order) > 0 {
-		sort.Slice(list, func(i, j int) bool {
-			defer func() {
-				if r := recover(); r != nil {
-					err = r.(error)
-				}
-			}()
-			for key, value := range order {
-				first, err := rowValue(list, i, key)
-				if err != nil {
-					panic(err)
-				}
-				second, err := rowValue(list, j, key)
-				if err != nil {
-					panic(err)
-				}
-				if second == nil {
-					return true
-				}
-				if first != nil && second != nil {
-					if fmt.Sprintf("%T", first) != fmt.Sprintf("%T", second) {
-						panic("type mismatch")
-					}
-				}
-				switch t := first.(type) {
-				case string:
-					{
-						if t != second {
-							second := second.(string)
-							if value {
-								return t < second
-							}
-							return t > second
-						}
-					}
-				case float64:
-					{
-						if t != second {
-							second := second.(float64)
-							if value {
-								return t < second
-							}
-							return t > second
-						}
-					}
-				case bool:
-					{
-						if t != second {
-							second := second.(bool)
-							firstVal := 0
-							if t {
-								firstVal = 1
-							}
-							secondVal := 0
-							if second {
-								secondVal = 1
-							}
-							if value {
-								return firstVal < secondVal
-							}
-							return firstVal > secondVal
-						}
-					}
-				case nil:
-					{
-						return false
-					}
-				default:
-					{
-						panic(sentinel.UNSUPPORTED_CASE)
-					}
-				}
+func Sort(slice []any, key string, desc bool) {
+	sort.Slice(slice, func(i, j int) bool {
+		_1 := slice[i].(map[string]any)[key]
+		if _1 == nil {
+			return false
+		}
+		_2 := slice[j].(map[string]any)[key]
+		if _2 == nil {
+			return true
+		}
+		if fmt.Sprintf("%T", _1) != fmt.Sprintf("%T", _2) {
+			return false
+		}
+		switch _1.(type) {
+		case float64:
+			{
+				return compare[float64](_1, _2) != desc
 			}
-			panic(sentinel.UNSUPPORTED_CASE)
+		case float32:
+			{
+				return compare[float32](_1, _2) != desc
+			}
+		case int64:
+			{
+				return compare[int64](_1, _2) != desc
+			}
+		case int:
+			{
+				return compare[int](_1, _2) != desc
+			}
+		case int32:
+			{
+				return compare[int32](_1, _2) != desc
+			}
+		case int16:
+			{
+				return compare[int16](_1, _2) != desc
+			}
+		case int8:
+			{
+				return compare[int8](_1, _2) != desc
+			}
+		case string:
+			{
+				return compare[string](_1, _2) != desc
+			}
+		case bool:
+			{
+				return compare[string](fmt.Sprintf("%v", _1), fmt.Sprintf("%v", _2)) != desc
+			}
+
+		}
+		return false
+	})
+}
+
+func compare[T float64 | float32 | int | int16 | int32 | int64 | int8 | string](first any, second any) bool {
+	_1 := first.(T)
+	_2 := second.(T)
+	return _1 > _2
+}
+
+type KeyValue struct {
+	Key   string
+	Value bool
+}
+
+func orderBy(__order map[string]bool, list []any) (err error) {
+	if len(__order) == 0 {
+		return nil
+	}
+	order := make([]KeyValue, 0)
+	for key, value := range __order {
+		order = append(order, KeyValue{
+			Key:   key,
+			Value: value,
 		})
+	}
+	Sort(list, order[0].Key, order[0].Value)
+	for idx := range order {
+		prev := "#"
+		bucket := make([]any, 0)
+		var firstIndex = 0
+		for index, i := range list {
+			var _key string
+			for _, key := range order[:idx] {
+				_key = _key + fmt.Sprintf("%v", i.(map[string]any)[key.Key])
+			}
+			if prev != "#" && _key != prev {
+				prev = "#"
+				Sort(bucket, order[idx].Key, order[idx].Value)
+				j := 0
+				for x := firstIndex; x < index; x++ {
+					list[x] = bucket[j]
+					j++
+				}
+				firstIndex = index
+				bucket = make([]any, 0)
+			}
+			prev = _key
+			bucket = append(bucket, i)
+		}
+		Sort(bucket, order[idx].Key, order[idx].Value)
+		j := 0
+		for x := firstIndex; x < len(list); x++ {
+			list[x] = bucket[j]
+			j++
+		}
 	}
 	return nil
 }
 
-func rowValue(list []any, index int, key string) (any, error) {
+// func orderBy(order map[string]bool, list []any) (err error) {
+// 	if len(order) > 0 {
+// 		sort.Slice(list, func(i, j int) bool {
+// 			defer func() {
+// 				if r := recover(); r != nil {
+// 					err = r.(error)
+// 				}
+// 			}()
+// 			for key, desc := range order {
+// 				first, err := rowdesc(list, i, key)
+// 				if err != nil {
+// 					panic(err)
+// 				}
+// 				if first == nil {
+// 					return false
+// 				}
+// 				second, err := rowdesc(list, j, key)
+// 				if err != nil {
+// 					panic(err)
+// 				}
+// 				if second == nil {
+// 					return true
+// 				}
+// 				if fmt.Sprintf("%T", first) != fmt.Sprintf("%T", second) {
+// 					panic("type mismatch")
+// 				}
+// 				switch t := first.(type) {
+// 				case string:
+// 					{
+// 						t2 := second.(string)
+// 						if t != t2 {
+// 							if desc {
+// 								return t < t2
+// 							}
+// 							return t > t2
+// 						}
+// 					}
+// 				case float64:
+// 					{
+// 						t2 := second.(float64)
+// 						if t != t2 {
+// 							if desc {
+// 								return t < t2
+// 							}
+// 							return t > t2
+// 						}
+// 						c := 10
+// 						_ = c
+// 					}
+// 				case int64:
+// 					{
+// 						t2 := second.(int64)
+// 						if t != t2 {
+// 							if desc {
+// 								return t < t2
+// 							}
+// 							return t > t2
+// 						}
+// 					}
+// 				case bool:
+// 					{
+// 						t2 := second.(bool)
+// 						if t != t2 {
+// 							firstVal := 0
+// 							if t {
+// 								firstVal = 1
+// 							}
+// 							secondVal := 0
+// 							if t2 {
+// 								secondVal = 1
+// 							}
+// 							if desc {
+// 								return firstVal < secondVal
+// 							}
+// 							return firstVal > secondVal
+// 						}
+// 					}
+// 				default:
+// 					{
+// 						panic(sentinel.UNSUPPORTED_CASE)
+// 					}
+// 				}
+// 			}
+// 			panic(sentinel.UNSUPPORTED_CASE)
+// 		})
+// 	}
+// 	return nil
+// }
+
+func rowdesc(list []any, index int, key string) (any, error) {
 	switch rowType := list[index].(type) {
 	case map[string]any:
 		{
