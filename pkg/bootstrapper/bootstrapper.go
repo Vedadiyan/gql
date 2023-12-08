e720ced94ac74da9fea4044144ead41314b7dc12
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-12-08 10:29:22 +0800

    function bootstrapper

---
+++ pkg/bootstrapper/bootstrapper.go
@@ -0,0 +1,26 @@
+package boostrapper
+
+import (
+	_ "github.com/vedadiyan/gql/pkg/functions/avg"
+	_ "github.com/vedadiyan/gql/pkg/functions/common"
+	_ "github.com/vedadiyan/gql/pkg/functions/concat"
+	_ "github.com/vedadiyan/gql/pkg/functions/count"
+	_ "github.com/vedadiyan/gql/pkg/functions/first"
+	_ "github.com/vedadiyan/gql/pkg/functions/max"
+	_ "github.com/vedadiyan/gql/pkg/functions/min"
+	_ "github.com/vedadiyan/gql/pkg/functions/mongo"
+	_ "github.com/vedadiyan/gql/pkg/functions/nullifempty"
+	_ "github.com/vedadiyan/gql/pkg/functions/once"
+	_ "github.com/vedadiyan/gql/pkg/functions/redis"
+	_ "github.com/vedadiyan/gql/pkg/functions/selectkey"
+	_ "github.com/vedadiyan/gql/pkg/functions/sum"
+	_ "github.com/vedadiyan/gql/pkg/functions/toarray"
+	_ "github.com/vedadiyan/gql/pkg/functions/tobytes"
+	_ "github.com/vedadiyan/gql/pkg/functions/todouble"
+	_ "github.com/vedadiyan/gql/pkg/functions/toint"
+	_ "github.com/vedadiyan/gql/pkg/functions/tomap"
+	_ "github.com/vedadiyan/gql/pkg/functions/tostring"
+	_ "github.com/vedadiyan/gql/pkg/functions/unwind"
+	_ "github.com/vedadiyan/gql/pkg/functions/uuid"
+	_ "github.com/vedadiyan/gql/pkg/functions/valueof"
+)
